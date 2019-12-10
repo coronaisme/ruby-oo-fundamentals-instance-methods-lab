57c5083bc75f5ca0e2b366ed740115bae638a2d7
Nick Corona <coronaisme@gmail.com>
2019-12-10 21:36:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
-class Dog 
-  def bark
-    puts "Woof!"
-  end 
-  def sit 
-    puts "The Dog is sitting"
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
   end
 end
